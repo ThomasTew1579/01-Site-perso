e1ceaa2ad66811d34495dabe1a4e2814f19e07e4
Thomas <pasquet.thomas69@gmail.com>
2022-09-09 15:37:31 +0200

    correction cartes ENCORE

--- JS/liste_cartes.js
+++ JS/liste_cartes.js
@@ -41,45 +41,61 @@ btnMenu.addEventListener("click", () => {
 // annimation carte section----------------------------------------------------
 // on peut faire plus simple en jQuery avec addClass et removeClass------------
 
-let carte = document.getElementsByClassName('carte')
-let text = carte.querySelector
+let contenuCarte = document.getElementsByClassName("contenu_carte");
+console.log(contenuCarte);
+let carte = document.getElementsByClassName("carte")
+console.log(carte[0].style.display)
 
 document.querySelector('.C1').addEventListener("click", () => {
-  for(const index in carte){
-    if(index < carte.length)
-    carte[index].style.flexGrow = "0";
-    document.querySelector(".contenu_carte").style.display = "none";
-    document.querySelector(".C1").style.flexGrow = 2;
-    document.querySelector(".contenu_carte").style.display = "flex";
+  for (const index in contenuCarte) {
+    if (index <= 3) {
+      carte[index].style.flexGrow = 0;                             
+      carte[0].style.flexGrow = 1;                              
+      contenuCarte[index]["style"]["display"] = "none";             
+      contenuCarte[0]["style"]["display"] = "flex";             
+      contenuCarte[index]["style"]["opacity"] = "0";             
+      contenuCarte[0]["style"]["opacity"] = "100%";             
+    }
   };
-
+  
 });
 
 document.querySelector('.C2').addEventListener("click", () => {
   for(const index in carte){
-    if(index < carte.length)
-    carte[index].style.flexGrow = "0";
-    carte[index].style.height = "60px";
-    document.querySelector(".C2").style.flexGrow = 2;
-    document.querySelector(".contenu_carte").style.display = "flex";
+    if (index <= 3) {
+      carte[index].style.flexGrow = 0;
+      carte[1].style.flexGrow = 1;
+      contenuCarte[index]["style"]["display"] = "none";
+      contenuCarte[1]["style"]["display"] = "flex";
+      contenuCarte[index]["style"]["opacity"] = "0";
+      contenuCarte[1]["style"]["opacity"] = "100%"; 
+    }
   };
 });
 
 document.querySelector('.C3').addEventListener("click", () => {
   for(const index in carte){
-    if(index < carte.length)
-    carte[index].style.flexGrow = "0";
-    document.querySelector(".C3").style.flexGrow = 2;
-    document.querySelector(".contenu_carte").style.display = "flex";
+    if (index <= 3) {
+      carte[index].style.flexGrow = 0;
+      carte[2].style.flexGrow = 1;
+      contenuCarte[index]["style"]["display"] = "none";
+      contenuCarte[2]["style"]["display"] = "flex";
+      contenuCarte[index]["style"]["opacity"] = "0";
+      contenuCarte[2]["style"]["opacity"] = "100%"; 
+    }
   };
 });
 
 document.querySelector('.C4').addEventListener("click", () => {
   for(const index in carte){
-    if(index < carte.length)
-    carte[index].style.flexGrow = "0";
-    document.querySelector(".C4").style.flexGrow = 2;
-    document.querySelector(".contenu_carte").style.display = "flex";
+    if (index <= 3) {
+      carte[index].style.flexGrow = 0;
+      carte[3].style.flexGrow = 1;
+      contenuCarte[index]["style"]["display"] = "none";
+      contenuCarte[3]["style"]["display"] = "flex";
+      contenuCarte[index]["style"]["opacity"] = "0";
+      contenuCarte[3]["style"]["opacity"] = "100%"; 
+    }
   };
 });
 
