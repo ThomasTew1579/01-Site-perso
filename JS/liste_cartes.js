4e850b93868de23e74661cb040c1ced699ba22f0
Thomas <pasquet.thomas69@gmail.com>
2022-09-13 11:29:12 +0200

    carte fini

--- JS/liste_cartes.js
+++ JS/liste_cartes.js
@@ -36,74 +36,103 @@ btnMenu.addEventListener("click", () => {
   }
 });
 
+let presentationMenu = document.querySelector(".S1")
+let experienceMenu = document.querySelector(".S2");
+let formationMenu = document.querySelector(".S3");
+let logicielsMenu = document.querySelector(".S4");
+
+presentationMenu.addEventListener("click", () => {
+  for(const index in section){
+    if(index <= section.length-1){
+      section[index].style.display="none";
+    }
+  }
+  menu.style.opacity = "100%";
+  menu.style.animationDuration = "1s";
+  menu.style.animationName = "disparition_menu";
+  listeCarte.style.animationName = "apparition";
+  listeCarte.style.display="flex";
+  setTimeout(() => {
+    menu.style.display = "none";
+  }, 1000);
+});
+
+experienceMenu.addEventListener("click", () => {
+  for (const index in section) {
+    if (index <= section.length - 1) {
+      section[index].style.display = "none";
+    }
+  };
+  menu.style.opacity = "100%";
+  menu.style.animationDuration = "1s";
+  menu.style.animationName = "disparition_menu";
+  listeCarte.style.animationDuration = "1s";
+  listeCarte.style.animationName = "disparition";
+  listeCarte.style.display = "none";
+  setTimeout(() => {
+    menu.style.display = "none";
+    section[0].style.display = "flex";
+  }, 1000);
+});
+
+formationMenu.addEventListener("click", () => {
+  for (const index in section) {
+    if (index <= section.length-1) {
+      section[index].style.display = "none";
+    }
+  };
+  menu.style.opacity = "100%";
+  menu.style.animationDuration = "1s";
+  menu.style.animationName = "disparition_menu";
+  listeCarte.style.animationDuration = "1s";
+  listeCarte.style.animationName = "disparition";
+  listeCarte.style.display = "none";
+  setTimeout(() => {
+    menu.style.display = "none";
+    section[2].style.display = "flex";
+  }, 1000);
+});
+
+logicielsMenu.addEventListener("click", () => {
+  for (const index in section) {
+    if (index <= section.length - 1) {
+      section[index].style.display = "none";
+    }
+  };
+  menu.style.opacity = "100%";
+  menu.style.animationDuration = "1s";
+  menu.style.animationName = "disparition_menu";
+  listeCarte.style.animationDuration = "1s";
+  listeCarte.style.animationName = "disparition";
+  listeCarte.style.display = "none";
+  setTimeout(() => {
+    menu.style.display = "none";
+    section[1].style.display = "flex";
+  }, 1000);
+});
+
+
 // --------------------------------------------------------------animation menu
 
 // annimation carte section----------------------------------------------------
 
-let contenuCarte = document.getElementsByClassName("contenu_carte");
-let carte = document.getElementsByClassName("carte")
-let listeCarte = document.querySelector(".liste_cartes");
-let section = document.getElementsByClassName("section");
+let contenuCarte      = document.getElementsByClassName("contenu_carte");
+let carte             = document.getElementsByClassName("carte")
+let listeCarte        = document.querySelector(".liste_cartes");
+let section           = document.getElementsByClassName("section");
 
 document.querySelector('.C2').addEventListener("click", () => {
   for(const index in carte){
-    if (index <= 3) {
+    if (index <= carte.length - 1) {
       carte[index].classList.remove("activeCarte");
       carte[index].classList.remove("background");
       carte[0].classList.add("activeCarte");
       carte[0].classList.add("background");
-      
+
       contenuCarte[index].classList.remove("activeContenu");
       contenuCarte[0].classList.add("activeContenu");
-      
     }
   };
-});
-
-document.querySelector('.C3').addEventListener("click", () => {
-  for(const index in carte){
-    if (index <= 3) {
-      carte[index].classList.remove("background");
-      carte[index].classList.remove("activeCarte");
-      carte[1].classList.add("activeCarte");
-      carte[1].classList.add("background");
-      
-      contenuCarte[index].classList.remove("activeContenu");
-      contenuCarte[1].classList.add("activeContenu");
-      
-    }
-  };
-});
-
-document.querySelector('.C4').addEventListener("click", () => {
-  for(const index in carte){
-    if (index <= 3) {
-      carte[index].classList.remove("background");
-      carte[index].classList.remove("activeCarte");
-      carte[2].classList.add("activeCarte");
-      carte[2].classList.add("background");
-      
-      contenuCarte[index].classList.remove("activeContenu");
-      contenuCarte[2].classList.add("activeContenu");
-      
-    }
-  };
-});
-
-document.querySelector('.C1').addEventListener("click", () => {
-  for (const index in contenuCarte) {
-    if (index <= 3) {
-      carte[index].classList.remove("background");
-      carte[index].classList.remove("activeCarte");
-      carte[3].classList.add("activeCarte");
-      carte[3].classList.add("background");
-      
-      contenuCarte[index].classList.remove("activeContenu");
-      contenuCarte[3].classList.add("activeContenu");
-          
-    }
-  };
-  
 });
 
 contenuCarte[0].addEventListener("click", () => {
@@ -115,6 +144,69 @@ contenuCarte[0].addEventListener("click", () => {
 
 },1000)
 })
+
+document.querySelector('.C3').addEventListener("click", () => {
+  for(const index in carte){
+    if (index <= carte.length - 1) {
+      carte[index].classList.remove("background");
+      carte[index].classList.remove("activeCarte");
+      carte[1].classList.add("activeCarte");
+      carte[1].classList.add("background");
+
+      contenuCarte[index].classList.remove("activeContenu");
+      contenuCarte[1].classList.add("activeContenu");
+    }
+  };
+});
+
+contenuCarte[1].addEventListener("click", () => {
+  listeCarte.style.animationDuration = "1s";
+  listeCarte.style.animationName = "disparition";
+  setTimeout(() => {
+    listeCarte.style.display = "none";
+    section[1].style.display = "flex";
+  }, 1000);
+});
+
+document.querySelector('.C4').addEventListener("click", () => {
+  for(const index in carte){
+    if (index <= carte.length - 1) {
+      carte[index].classList.remove("background");
+      carte[index].classList.remove("activeCarte");
+      carte[2].classList.add("activeCarte");
+      carte[2].classList.add("background");
+
+      contenuCarte[index].classList.remove("activeContenu");
+      contenuCarte[2].classList.add("activeContenu");
+    }
+  };
+});
+
+contenuCarte[0].addEventListener("click", () => {
+  listeCarte.style.animationDuration = "1s";
+  listeCarte.style.animationName = "disparition";
+  setTimeout(() => {
+    listeCarte.style.display = "none";
+    section[0].style.display = "flex";
+  }, 1000);
+});
+
+document.querySelector('.C1').addEventListener("click", () => {
+  for (const index in contenuCarte) {
+    if (index <= carte.length - 1) {
+      carte[index].classList.remove("background");
+      carte[index].classList.remove("activeCarte");
+      carte[3].classList.add("activeCarte");
+      carte[3].classList.add("background");
+
+      contenuCarte[index].classList.remove("activeContenu");
+      contenuCarte[3].classList.add("activeContenu");
+    }
+  };
+  
+});
+
+
 
 // ----------------------------------------------------annimation carte section
 
