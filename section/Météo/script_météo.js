8a1bcda611e8e0f9eebcc200783fde468dc2cc75
Yevic <yevic.tabita@opera-energie.com>
2022-09-22 11:07:19 +0200

    Suggestion d'amélioration

--- section/Météo/script_météo.js
+++ section/Météo/script_météo.js
@@ -4,7 +4,8 @@ recevoirTemperature(villeChoisie);
 let changerDeVille = document.querySelector("#changer");
 changerDeVille.addEventListener("click", () => {
   villeChoisie = prompt("Quelle ville souhaitez-vous voir ?");
-  recevoirTemperature(villeChoisie);
+  let temperature = recevoirTemperature(villeChoisie);
+  afficherTemperature(temperature.ville, temperature.temperature)
 });
 
 function recevoirTemperature(ville) {
@@ -13,7 +14,7 @@ function recevoirTemperature(ville) {
     ville +
     "&appid=dc8c9153e8adaad0ec8bf635818c0d42&units=metric";
 
-  let requete = new XMLHttpRequest();
+  let requete = new XMLHttpRequest();//Tu peux aussi utiliser fetch pour les requetes HTTP (plus populaire de nos jour je crois)
   requete.open("GET", url);
   requete.responseType = "json";
   requete.send();
@@ -21,14 +22,32 @@ function recevoirTemperature(ville) {
   requete.onload = function () {
     if (requete.readyState === XMLHttpRequest.DONE) {
       if (requete.status === 200) {
-        let reponse = requete.response;
-        let temperature = reponse.main.temp;
-        let ville = reponse.name;
-        document.querySelector("#temperature_label").textContent = temperature;
-        document.querySelector("#ville").textContent = ville;
+        /* Tout ce traiement je l'aurai bien vu dans une fonction
+        * AffichierTemperature(ville,temperature)
+        * Le but est de respecter la regle du single responsability
+        * Car ici ta fonction recevoirTemperature fait aussi de l'affichage, si un collegue l'utilise il ne s'attend
+        * pas a ce que cette fonction modifie le DOM.
+        */
+        //let reponse = requete.response;
+        //let temperature = reponse.main.temp;
+        //let ville = reponse.name;
+        //document.querySelector("#temperature_label").textContent = temperature;
+        //document.querySelector("#ville").textContent = ville;
+        return {
+          ville: requete.reponse.name,
+          temperature: requete.reponse.name.temp
+        }
       } else {
+        //En cas de probleme tu pourrai analyser la reponse quand meme pour donner du context a l'utilisateur (ex ville introuvable)
+        //Ou les 401 ^^
         alert("Un problème est intervenu, merci de revenir plus tard.");
       }
     }
   };
+}
+
+function afficherTemperature(ville, temperature){
+  //ton traitement d'affichage
+  document.querySelector("#temperature_label").textContent = temperature;
+  document.querySelector("#ville").textContent = ville;
 }
